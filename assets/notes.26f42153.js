d6237df770452e9f3a449c7ebff9fac595dbdfbc
AA961 <ammar.ahmed961@gmail.com>
2022-08-14 19:25:44 +0500

    Fixing Transitions

---
+++ assets/notes.26f42153.js
@@ -0,0 +1,1 @@
+var e=[{name:"Discreet Notes",fileName:"MTH202_handouts_1_45.pdf",subjecCode:"MTH202",class:"BS"},{name:"Demo Notes",fileName:"demo.pdf",subjecCode:"DEMO101",class:"DEMO"}];export{e as n};
