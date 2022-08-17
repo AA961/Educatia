17d2457023597c11493075e02d03d3d93d133065
AA961 <ammar.ahmed961@gmail.com>
2022-08-17 16:24:35 +0500

    Adding Univercities Filter

---
+++ assets/notes.9ac0241c.js
@@ -0,0 +1,1 @@
+const e=[{name:"Discreet Notes",fileName:"MTH202_handouts_1_45.pdf",subjecCode:"MTH202",class:"BS"}];export{e as n};
--- assets/notes.aa3aa818.js
+++
@@ -1,1 +0,0 @@
-var e=[{name:"Discreet Notes",fileName:"MTH202_handouts_1_45.pdf",subjecCode:"MTH202",class:"BS"},{name:"Demo Notes",fileName:"demo.pdf",subjecCode:"DEMO101",class:"DEMO"}];export{e as n};
